2d0e536ca50a358373e4bf351815a6fe765156e4
SWATHI-63 <dhivagarswathi@gmail.com>
2026-01-13 21:05:14 +0530

    variable-changing

--- palindrome.java
+++ palindrome.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class palindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
+        String a = sc.nextLine();
         String reversedStr = "";
 
-        for (int i = str.length() - 1; i >= 0; i--) {
-            reversedStr += str.charAt(i);
+        for (int i = a.length() - 1; i >= 0; i--) {
+            reversedStr += a.charAt(i);
         }
 
-        if (str.equals(reversedStr)) {
+        if (a.equals(reversedStr)) {
             System.out.println("true");
         } else {
             System.out.println("false");
