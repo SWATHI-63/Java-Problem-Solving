2e1d4bd427519fcd2c5f20bccb17696006b80be8
SWATHI-63 <dhivagarswathi@gmail.com>
2026-01-16 22:33:36 +0530

    armstrong

---
+++ armstrong.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class armstrong {
+    public static boolean isArmstrong(int n) {
+        int originalNumber = n;
+        int sum = 0;
+        int digits = String.valueOf(n).length();
+
+        while (n != 0) {
+            int digit = n % 10;
+            sum += Math.pow(digit, digits);
+            n /= 10;
+        }
+
+        return sum == originalNumber;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+    }
+}
